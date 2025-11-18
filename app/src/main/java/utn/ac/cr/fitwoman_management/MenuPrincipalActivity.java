f164664e5fddca1c1d64ce5eecbf3f66e7c8a584
unknown <kadiazsa@est.utn.ac.cr>
2025-11-17 19:57:49 -0600

    actializacion del CRUD y correcciones

--- app/src/main/java/utn/ac/cr/fitwoman_management/MenuPrincipalActivity.java
+++ app/src/main/java/utn/ac/cr/fitwoman_management/MenuPrincipalActivity.java
@@ -1,25 +1,59 @@
 package utn.ac.cr.fitwoman_management;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 
 public class MenuPrincipalActivity extends AppCompatActivity {
+
+    private Button btnMisEntrenamientos;
+    private Button btnFotosProgreso;
+    private Button btnEstadisticas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu_principal);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        initializeViews();
+        setupListeners();
+    }
+
+    private void initializeViews() {
+        btnMisEntrenamientos = findViewById(R.id.btnMisentrenamientos);
+        btnFotosProgreso = findViewById(R.id.btnfotosProgreso);
+        btnEstadisticas = findViewById(R.id.btnestadisticas);
+    }
+
+    private void setupListeners() {
+        // Button: My Workouts
+        btnMisEntrenamientos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuPrincipalActivity.this, ListaEntrenamientoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Button: Progress Photos
+        btnFotosProgreso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuPrincipalActivity.this, fotosProgreso.class);
+                startActivity(intent);
+            }
+        });
+
+        // Button: Statistics
+        btnEstadisticas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuPrincipalActivity.this, Estadisticas_Activity.class);
+                startActivity(intent);
+            }
         });
     }
 }
